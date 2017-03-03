d766bdddc576781da528074599c019c790024471
matthew-zak <matthew.zak@tooploox.com>
2017-03-02 19:22:56 -0500

    Add convolutional layer and create encoder(generator) scratch

---
+++ experiments/layers/convolutional_layer.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+from math import sqrt
+import numpy as np
+import theano
+import theano.tensor as T
+from theano.tensor.nnet import conv2d
+from theano.tensor.signal import pool
+
+theano.config.floatX = 'float32'
+
+class ConvolutionalLayer(object):
+    
+    def __init__(self, input, filter_shape, image_shape, poolsize=(2,2)):
+        """
+        Convolution layer with pooling
+        
+        :type input: theano.tensor.dtensor4
+        :param input: symbolic image tensor of shape image_shape
+        
+        :type filter_shape: a tuple or a list of 4. elements
+        :param filter_shape: (number of filters, number of input feature maps,
+                              filter height, filter width)
+        
+        :type image_shape: a tuple of a list of 4. elements
+        :param image_shape: (batch size, number of channels or feature maps,
+                             image height, image width)
+        
+        :type poolsize: a tuple or a list of 2 elements
+        :param poolsize: pooling size - downsampling. (width, height)
+        """
+        
+        assert image_shape[1] == filter_shape[1]
+        self.input = input
+        
+        # number of input to a feature map
+        n_in = np.prod(image_shape[2:])
+        W = np.asanyarray(
+                np.random.rand(size = filter_shape) * sqrt(2.0/n_in),
+                dtype = theano.config.floatX
+                )
+        # One bias per feature map
+        b = np.zeros((filter_shape[0], ), dtype = theano.config.floatX)
+        
+        self.W = theano.shared(W, borrow = True)
+        self.b = theano.shared(b, borrow = True)
+        
+        # Convolve input feature maps with filters
+        # conv2d from tensor.nnet implements the convolutional layers 
+        # present in convolutional neural networks
+        # (where filters are 3D and pool over several input channels)
+        self.convolution_output = conv2d(
+                input = input,
+                filters = self.W,
+                input_shape = image_shape,
+                filter_shape = filter_shape,
+                border_mode = filter_shape[2] - 1
+                )
+        
+        if poolsize is not None:
+            # pool each feature map individually using maxpooling
+            self.pooled_out = pool.pool_2d(
+                    input = self.convolution_output,
+                    ds = poolsize,
+                    ignore_border = True
+                    )
+            
+        # calculate the output of the layer
+        #self.output = T.nnet.relu(pooled_out + 
+        #                          self.b.dimshuffle('x', 0, 'x', 'x'))
+        
+        # parameters of the layer
+        self.params = [self.W, self.b]
+
+    #def output(activation)
+        
+if __name__ == '__main__':
+    cl = ConvolutionalLayer('ala', 'blabl')
+    print(cl.input, cl.temp)
---
+++ experiments/layers/generator.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+from convolutional_layer import ConvolutionalLayer
+from math import sqrt
+import numpy as np
+import theano.tensor as T
+
+class Generator(object):
+    
+    def __init__(input, batch_size):
+        x = T.matrix('x')
+        y = T.matrix('y')
+        
+        #############
+        ## Encoder ##
+        #############
+        
+        input_layer = x.reshape((batch_size, 3, 64, 64))
+        
+        # Construct the first convolutional pooling layer:
+        # filtering reduces the image size to (64 , 64) - image padded
+        # maxpooling reduces this further to (64/2, 64/2) = (32, 32)
+        # 4D output tensor is thus of shape (batch_size, 64, 32, 32)
+        layer_0 = ConvolutionalLayer(
+                input = input_layer,
+                filter_shape = (64, 3, 4, 4),
+                image_shape = (batch_size, 3, 64, 64),
+                poolsize = (2, 2)
+                )
+        # create output for Convolutional layer
+        # LeakyReLU alpha = 0.2
+        layer_0_output = None
+        
+        # maxpooling reduces the size (32 / 2, 32 / 2) = (16, 16)
+        # filter shape: # of filters, filter depth, length, width
+        # filter depth should be equal to the input depth shape[1]
+        layer_1 = ConvolutionalLayer(
+                input = layer_0_output, 
+                filter_shape = (128, 64, 4, 4),
+                image_shape = (batch_size, 64, 32, 32),
+                poolsize = (2, 2)
+                )
+        
+        # create output for Convolutional layer
+        # Batch normalisation
+        # LeakyReLU alpha = 0.2
+        layer_1_batch_output = None
+        layer_1_output = None
+        
+        # maxpooling reduces the size (16 / 2, 16 / 2) = (8, 8)
+        # filter shape: # of filters, input depth, filter shape
+        layer_2 = ConvolutionalLayer(
+                input = layer_1_output,
+                filter_shape = (256, 128, 4, 4),
+                image_shape = (batch_size, 128, 16, 16),
+                poolsize = (2, 2)
+                )
+        
+        # Batch normalisation
+        layer_2_batch_output = None
+        # LeakyReLU
+        layer_2_output = None
+        
+        # maxpooling reduces the size (8 / 2, 8 / 2) = (4, 4)
+        layer_3 = ConvolutionalLayer(
+                input = layer_2_output,
+                filter_shape = (512, 256, 4, 4),
+                image_shape = (batch_size, 256, 8, 8),
+                poolsize = (2, 2)
+                )
+        
+        # Batch normalisation
+        layer_3_batch_norm = None
+        # LeakyReLU
+        layer_3_output = None
+
+        # maxpooling reduces the size (4 / 2, 4 / 2) = (2, 2)        
+        layer_4 = ConvolutionalLayer(
+                input = layer_3_output,
+                filter_shape = (512, 512, 4, 4),
+                image_shape = (batch_size, 512, 4, 4),
+                poolsize = (2, 2)
+                )
+        
+        # Batch normalisation
+        layer_4_batch_norm = None
+        # LeakyReLU
+        layer_4_output = None
+        
+        # maxpooling reduces the size (2 / 2, 2 / 2) = (1, 1)        
+        layer_5 = ConvolutionalLayer(
+                input = layer_3_output,
+                filter_shape = (512, 512, 4, 4),
+                image_shape = (batch_size, 512, 4, 4),
+                poolsize = (2, 2)
+                )
+        
+        # Batch normalisation
+        layer_5_batch_norm = None
+        # LeakyReLU
+        layer_5_output = None
+        
+        #############
+        ## Decoder ##
+        #############
