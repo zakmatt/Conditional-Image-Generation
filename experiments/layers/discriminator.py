a4e4cb1899bd09027fef9aa8e17fcd691a48c705
matthew-zak <matthew.zak@tooploox.com>
2017-03-12 21:24:56 -0400

    discriminator initial commit

---
+++ experiments/layers/discriminator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class Discriminator(object):
+    def __init__(self, input, batch_size):
+        
+        self.dropout_layers = []
+        self.layers = []
+    
+if __name__ == '__main__':
+    pass
