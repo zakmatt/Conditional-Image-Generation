eb54832ba3fc055c59d140f2c46aca725840ea58
matthew-zak <matthew.zak@tooploox.com>
2017-03-18 16:48:35 -0400

    predict true and fake

--- experiments/layers/discriminator.py
+++ experiments/layers/discriminator.py
@@ -61,10 +61,9 @@ class Discriminator(object):
                 subsample = (1, 1)
                 )
         self.layers.append(layer_3)
-        self.discriminator_output = self.layers[-1].output(activation = 'sigm')
         self.params = [param for layer in self.layers 
                        for param in layer.params]
-    '''   
+        
     def output(self, activation):
         if activation == 'tanh':
             self.discriminator_output = self.layers[-1].output(activation = 'tanh')
@@ -76,75 +75,23 @@ class Discriminator(object):
             self.discriminator_output = self.layers[-1].output(activation = None)
             
         return self.discriminator_output
-    '''
-    def output(self):
-        return self.discriminator_output
     
-    def create_predict(self):
-        contour = T.tensor4('contour')
-        target = T.tensor4('target')
+    def _predict_score(self, contour, target):
         predict = theano.function(
-                [contour, target],
-                self.output('sigm'),
+                [],
+                self.discriminator_output,
                 givens = {
                         disc_corrupted: contour,
                         disc_input: target
                         }
                 )
-        self.predict = predict
+        return predict()
 
-'''
-def discriminator(batch_size, discriminator_targets, discriminator_inputs = None):
-    lrelu = lasagne.nonlinearities.LeakyRectify(0.2)
-    #discriminator_inputs = T.tensor4('discriminator_inputs')
-    #discriminator_inputs = discriminator_inputs.reshape((batch_size, 3, 64, 64))
-    #input_layer = T.concatenate([discriminator_inputs, discriminator_targets], axis = 1)
-    #discriminator_inputs = lasagne.layers.InputLayer(shape=(batch_size, 3, 64, 64),
-    #                                                 input_var=discriminator_inputs)
-    #discriminator_targets = lasagne.layers.InputLayer(shape=(batch_size, 3, 64, 64),
-    #                                                 input_var=discriminator_targets)
-    network = lasagne.layers.ConcatLayer([discriminator_inputs, discriminator_targets], axis = 1)
-    network = lasagne.layers.InputLayer(shape=(batch_size, 6, 64, 64),
-                                        input_var = network)
-    # Construct the first convolutional layer:
-    # filtering reduces the image size to (64 , 64) - image padded
-    # subsampling this further to (64/2, 64/2) = (32, 32)
-    # 4D output tensor is thus of shape (batch_size, 64, 32, 32)
-    network = lasagne.layers.Conv2DLayer(network, num_filters=64, filter_size=(5, 5),
-                                         stride=2, nonlinearity=lrelu)
-    network = lasagne.layers.BatchNormLayer(network)
-    
-    network = lasagne.layers.Conv2DLayer(network, num_filters=128, filter_size=(5, 5),
-                                         stride=2, nonlinearity=lrelu)
-    network = lasagne.layers.BatchNormLayer(network)
-    
-    network = lasagne.layers.Conv2DLayer(network, num_filters=256, filter_size=(5, 5),
-                                         stride=2, nonlinearity=lrelu)
-    network = lasagne.layers.BatchNormLayer(network)
-    
-    network = lasagne.layers.Conv2DLayer(network, num_filters=30, filter_size=(5, 5),
-                                         stride=1, nonlinearity=lasagne.nonlinearities.sigmoid)
-    
-    #self.network = network
-    #self.get_output_for = network
-    return network
-        
-def _get_disc_score(disc, discriminator_inputs):
-    return lasagne.layers.get_output(disc.layers, discriminator_inputs)
-    
-def get_loss(disc_real, disc_sample):
-
-    #WGAN loss
-    discriminator_loss = -(T.log(disc_real) + T.log(1.-disc_sample)).mean()
-    generator_loss = -T.log(disc_sample).mean()
-
-    return discriminator_loss, generator_loss
-'''     
 if __name__ == '__main__':
     import numpy as np
     import theano
     theano.config.floatX = 'float32'
-    
+        
     full_images = np.random.randn(30, 3, 64, 64) * 100
     full_images = theano.shared(value = np.asanyarray(full_images, dtype = theano.config.floatX))
     
@@ -168,15 +115,14 @@ if __name__ == '__main__':
     disc_corrupted = T.tensor4('disc_corrupted')
     disc_corrupted_images = disc_corrupted.reshape((30, 3, 64, 64))
     
-    #discriminator = Discriminator(disc_input_images, generator_output, 30)
     discriminator = Discriminator(disc_corrupted_images, disc_target_images, 30)
-    #discriminator.create_predict()
-    
-    #cont = T.tensor4('cont')
-    #target = T.tensor4('target')
+    discriminator.output('sigm')
+    pred_fake = discriminator._predict_score(contour, generator_output)
+    pred_real = discriminator._predict_score(contour, full_images)
+    '''
     predict_fake = theano.function(
             [],
-            discriminator.output(),
+            disc_output,
             givens = {
                     disc_corrupted: contour,
                     disc_input: generator_output
@@ -185,7 +131,7 @@ if __name__ == '__main__':
     
     predict_true = theano.function(
             [],
-            discriminator.output(),
+            disc_output,
             givens = {
                     disc_corrupted: contour,
                     disc_input: full_images
@@ -194,84 +140,6 @@ if __name__ == '__main__':
     
     pred_fake = predict_fake()
     pred_real = predict_true()
-    #predict_fake = discriminator.predict(contour, generator_output)
-    #predict_real = discriminator.predict(contour, full_images)
+    '''
     print('fake shape: ', pred_fake.shape)
     print('real shape: ', pred_real.shape)
-    '''
-
-    inputss = np.random.randn(30, 3, 64, 64) * 100
-    inputss = np.asarray(inputss, dtype = theano.config.floatX)
-    inputss = theano.shared(value = inputss,
-                                borrow = True)
-    contour = T.set_subtensor(inputss[:, :, 16:32, 16:32], 0)
-    contour_input = T.tensor4('contour_input')
-    contour_input_image = contour_input.reshape((30, 3, 64, 64))
-    generator = Generator(contour_input_image, 30)
-    gen_output = generator.output('tanh')
-    
-    input_image = T.tensor4('input_image')
-    discriminator_input = input_image.reshape((30, 3, 64, 64))
-
-    oryginal_image = T.tensor4('oryginal_image')
-    discriminator_target = input_image.reshape((30, 3, 64, 64))
-    
-    dis = Discriminator(contour_input_image, discriminator_target, 30)
-    
-    predict_fake = theano.function(
-                [],
-                dis.output('sigm'),
-                givens = {
-                        contour_input: contour,
-                        discriminator_target: gen_output
-                }
-            )
-    
-    
-    #discr_input = gen_output
-
-    d_i = T.tensor4('d_i')
-    d_i = d_i.reshape((30, 3, 64, 64))
-    discriminator = Discriminator(d_i, input_x, 30)
-    b = theano.function(
-            [],
-            discriminator.output('sigm'),
-            givens = {
-                    d_i: discr_input,
-                    x: inputss
-                    }
-            )
-    discriminator_output = b()
-    print(discriminator_output.shape)
-
-    # x is contour
-    # d_i is contour
-    # oryginal image is inputss
-    # fake image is gen_output
-    d_i = T.tensor4('d_i')
-    d_i = d_i.reshape((30, 3, 64, 64))
-    disc = discriminator(30, d_i)
-    #fake = discriminator._get_disc_score(gen_output)
-    #oryginal_image = T.tensor4('oryginal_image')
-    #oryginal_image = oryginal_image.reshape((30, 3, 64, 64))
-    origin_real_img = T.tensor4('real_img')
-    real_img = origin_real_img.dimshuffle((0, 3, 1, 2))
-    #oryginal_image = oryginal_image.dimshuffle((0, 3, 1, 2))
-    #fake_image = T.tensor4('fake_image')
-    #fake_image = fake_image.reshape((30, 3, 64, 64))
-    predict_real = _get_disc_score(disc, real_img)
-    predict_fake = _get_disc_score(disc, gen_output)
-    
-    # Our loss
-    discriminator_loss, generator_loss = get_loss(predict_real, predict_fake)
-
-    #Discriminator update
-    discriminator_params = lasagne.layers.get_all_params(disc, trainable=True)
-    discriminator_updates = lasagne.updates.adam(discriminator_loss, discriminator_params, learning_rate=0.1, beta1=0.5)
-
-    train_discriminator_fn = theano.function([input_x + origin_real_img], [discriminator_loss],
-                                             updates=discriminator_updates,
-                                             allow_input_downcast=True)
-
-    xyz = train_discriminator_fn(contour, inputss)
-    '''
