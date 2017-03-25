08c9803e00fbfa6187c50681c3b38325fd6ad9a9
matthew-zak <matthew.zak@tooploox.com>
2017-03-25 16:51:40 -0400

    Create batches of full dataset as shared variables and use micro-batches to iterate through a batch

--- experiments/train.py
+++ experiments/train.py
@@ -11,33 +11,12 @@ import theano
 import theano.tensor as T
 theano.config.floatX = 'float32'
 
-BATCH_SIZE = 64
+BATCH_SIZE = 128
+MICRO_BATCH_SIZE = 32
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def train_model(full_images, full_validate, batch_size, method, save_dir, n_epochs=200):
-    
-    corrupted_images = T.set_subtensor(full_images[:, :, 16:48, 16:48],  0)
-    corrupted_validate = T.set_subtensor(full_validate[:, :, 16:48, 16:48],  0)
-    oryginal_images  = full_images.get_value(borrow=True)
-    
-    # compute number of minibatches for training
-    n_train_batches = full_images.get_value(borrow=True).shape[0]
-    n_train_batches //= BATCH_SIZE
-    
-    # allocate symbolic variables for the data
-    index = T.lscalar()  # index to a [mini]batch
-    
-    corrupted_images_T = T.tensor4('corrupted_images')
-    corrupted_input_images_T = corrupted_images_T.reshape((BATCH_SIZE, 3, 64, 64))
-    
-    full_images_T = T.tensor4('full_images')
-    full_input_images_T = full_images_T.reshape((BATCH_SIZE, 3, 64, 64))
-    
-    model = Model(corrupted_input_images_T, full_input_images_T, BATCH_SIZE)
-    
-    discrm_cost = model.discriminator_loss()
-    gen_cost = model.generator_loss(1.0, 100.0)
-    
+
+def set_updaters(method):
     if method == 'adam':
         generator_updater = Adam(lr=2.0, 
                                  b1=0.5, 
@@ -64,6 +43,36 @@ def train_model(full_images, full_validate, batch_size, method, save_dir, n_epoc
                                     b1=0.5, 
                                     clipnorm=10., 
                                     regularizer=Regularizer(l2=1e-5))
+        
+    return generator_updater, discriminator_updater
+
+def train_model(full_images, full_validate, batch_size, method, save_dir, n_epochs = 200):
+    # compute number of minibatches for training
+    n_train_batches = full_images.shape[0]
+    n_train_batches //= BATCH_SIZE
+    n_train_micro_batches = BATCH_SIZE // MICRO_BATCH_SIZE
+    
+    # macro batch
+    macro_batch_full_images = theano.shared(np.empty((BATCH_SIZE, ) + full_images.shape[1:], dtype = theano.config.floatX),
+                                            borrow = True)
+    macro_batch_corrupted_images = theano.shared(np.empty((BATCH_SIZE, ) + full_images.shape[1:], dtype = theano.config.floatX),
+                                            borrow = True)
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a microbatch
+    corrupted_images_T = T.tensor4('corrupted_images')
+    corrupted_input_images_T = corrupted_images_T.reshape((MICRO_BATCH_SIZE, 3, 64, 64))
+    
+    full_images_T = T.tensor4('full_images')
+    full_input_images_T = full_images_T.reshape((MICRO_BATCH_SIZE, 3, 64, 64))
+    
+    model = Model(corrupted_input_images_T, full_input_images_T, MICRO_BATCH_SIZE)
+    
+    discrm_cost = model.discriminator_loss()
+    gen_cost = model.generator_loss(1.0, 100.0)
+    
+    # set update methods
+    generator_updater, discriminator_updater = set_updaters(method)
+    
     gen_params = model.generator.params
     gen_updates = generator_updater(gen_params, gen_cost)
     dis_params = model.discriminator_real.params
@@ -74,8 +83,8 @@ def train_model(full_images, full_validate, batch_size, method, save_dir, n_epoc
             gen_cost,
             updates = gen_updates,
             givens = {
-                    corrupted_images_T: corrupted_images[index * batch_size:(index + 1) * batch_size],
-                    full_images_T: full_images[index * batch_size:(index + 1) * batch_size]
+                    corrupted_images_T: macro_batch_corrupted_images[index * MICRO_BATCH_SIZE :(index + 1) * MICRO_BATCH_SIZE],
+                    full_images_T: macro_batch_full_images[index * MICRO_BATCH_SIZE:(index + 1) * MICRO_BATCH_SIZE]
                     }
             )
     train_discriminator_fn = theano.function(
@@ -83,8 +92,8 @@ def train_model(full_images, full_validate, batch_size, method, save_dir, n_epoc
             discrm_cost,
             updates = dis_updates,
             givens = {
-                    corrupted_images_T: corrupted_images[index * batch_size:(index + 1) * batch_size],
-                    full_images_T: full_images[index * batch_size:(index + 1) * batch_size]
+                    corrupted_images_T: macro_batch_corrupted_images[index * MICRO_BATCH_SIZE:(index + 1) * MICRO_BATCH_SIZE],
+                    full_images_T: macro_batch_full_images[index * MICRO_BATCH_SIZE:(index + 1) * MICRO_BATCH_SIZE]
                     }
             )
 
@@ -92,7 +101,7 @@ def train_model(full_images, full_validate, batch_size, method, save_dir, n_epoc
             [index],
             model.generate_image,
             givens = {
-                    corrupted_images_T: corrupted_images[index * batch_size:(index + 1) * batch_size]
+                    corrupted_images_T: macro_batch_corrupted_images[index * MICRO_BATCH_SIZE:(index + 1) * MICRO_BATCH_SIZE]
                     }
             )
     
@@ -100,32 +109,49 @@ def train_model(full_images, full_validate, batch_size, method, save_dir, n_epoc
             [index],
             model.generate_image,
             givens = {
-                    corrupted_images_T: corrupted_validate[index * batch_size:(index + 1) * batch_size]
+                    corrupted_images_T: macro_batch_corrupted_images[index * MICRO_BATCH_SIZE:(index + 1) * MICRO_BATCH_SIZE]
                     }
-            )    
+            )
+    
     # train
     epoch = 0
     logging.info('start training')
     while (epoch < n_epochs):
         epoch += 1
         logging.info('epoch: %d' % epoch)
-
-        for minibatch_index in range(n_train_batches):
-            iter = (epoch - 1) * n_train_batches + minibatch_index
-            logging.info('iter: %d' % iter)
-
-            cost_gen = train_generator_fn(minibatch_index)
-            cost_disc = train_discriminator_fn(minibatch_index)
-                   
+        
+        for batch_index in range(n_train_batches):
+            
+            # Create a shared variable of a batch
+            macro_batch_full_images.set_value(
+                    full_images[batch_index * batch_size:(batch_index + 1) * batch_size],
+                    borrow = True
+                    )
+            macro_batch_corrupted_images.set_value(
+                    T.set_subtensor(macro_batch_full_images[:, :, 1:4, 1:4],  0).eval(),
+                    borrow = True
+                    )
+            
+            # Iterate throught a batch by taking micro batches
+            total_gen_cost = 0.
+            total_disc_cost = 0.
+            for micro_batch_index in range(n_train_micro_batches):
+                total_gen_cost += train_generator_fn(micro_batch_index)
+                total_disc_cost += train_discriminator_fn(micro_batch_index)
+                
+            total_gen_cost /= n_train_micro_batches
+            total_disc_cost /= n_train_micro_batches
+            
+            iter = (epoch - 1) * n_train_batches + batch_index
             if iter % 100 == 0:
                 logging.info('training @ iter = %s' % iter)
-                logging.info('generator cost = %.3f' % cost_gen)
-                logging.info('discriminator cost = %.3f' % cost_disc)
-
+                logging.info('generator cost = %.3f' % total_gen_cost)
+                logging.info('discriminator cost = %.3f' % total_disc_cost)
+        
         # put images through the network, reshape and save 
         if epoch % 20 == 0:
-            current_images = oryginal_images[minibatch_index * batch_size:(minibatch_index + 1) * batch_size]
-            pred = generate_train(minibatch_index)
+            current_images = macro_batch_full_images.get_value(borrow = True)[micro_batch_index * MICRO_BATCH_SIZE:(micro_batch_index + 1) * MICRO_BATCH_SIZE]
+            pred = generate_train(micro_batch_index)
             file_name = 'train_generated_%s_%d.npy' % (method, epoch)
             np.save(os.path.join(save_dir, file_name), pred)
             
@@ -133,13 +159,13 @@ def train_model(full_images, full_validate, batch_size, method, save_dir, n_epoc
             file_name = 'train_generated_filled_%s_%d.npy' % (method, epoch)
             np.save(os.path.join(save_dir, file_name), pred)
             
-            pred = generate_validation(minibatch_index)
+            pred = generate_validation(micro_batch_index)
             file_name = 'validate_generated_%s_%d.npy' % (method, epoch)
             np.save(os.path.join(save_dir, file_name), pred)
             pred = fill_missing_part(current_images, pred)
             file_name = 'validate_generated_filled_%s_%d.npy' % (method, epoch)
             np.save(os.path.join(save_dir, file_name), pred)
-            
+
     # saving the model
     logging.info('Saving...')
     params = model.generator.model_params
