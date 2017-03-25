4a06e21c39e1fcf457af5000e5e50d153ea6e410
matthew-zak <matthew.zak@tooploox.com>
2017-03-25 16:55:53 -0400

    change runscript

--- experiments/run_script/run_script.sh
+++ experiments/run_script/run_script.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-# PBS -l nodes=1:ppn=2 -l mem=6gb
-# PBS -l walltime=168:00:00
-# PBS -N mzak-ift6266
-# PBS -o /home2/ift6ed68/logs.out
-# PBS -M zak.matthew@yahoo.com
-# PBS -m abe
-# PBS -q test
-# PBS -r n
-# PBS -j oe
-# PBS -V
-# --------------------------------------------------------------
+#PBS -l nodes=1:ppn=2 -l mem=6gb
+#PBS -l walltime=00:30:00
+#PBS -N mzak-ift6266
+#PBS -o /home2/ift6ed68/logs.out
+#PBS -M zak.matthew@yahoo.com
+#PBS -m abe
+#PBS -r n
+#PBS -j oe
+#PBS -V
+#--------------------------------------------------------------
 module add python/3.5.1
 module add CUDA/7.5
 
 source /home2/ift6ed68/pyt3.5/bin/activate
 cd /home2/ift6ed68/Conditional-Image-Generation/
-THEANO_FLAGS='floatX=float32,device=gpu' python train.py -d experiments/dataset -s experiments/
+THEANO_FLAGS='floatX=float32,device=gpu' python experiments/train.py -d experiments/dataset -s experiments/
